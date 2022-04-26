7d4d51efce22064b2783a6b6a2a1d66b3618d71d
RecycleBee <83790689+RecycleBee@users.noreply.github.com>
2022-04-26 14:29:15 -0700

    Personalized training scores

--- auto_derby/single_mode/training/training_score.py
+++ auto_derby/single_mode/training/training_score.py
@@ -13,20 +13,12 @@ if TYPE_CHECKING:
 
 
 def compute(ctx: Context, trn: Training) -> float:
-    t_now = ctx.turn_count_v2()
     spd = mathtools.integrate(
         ctx.speed,
         trn.speed,
-        (
-            (0, 2.0),
-            (300, 1.0),
-            (600, 0.8),
-            (900, 0.7),
-            (1100, 0.5),
-            (1120, 0.1),
-        ),
+        ((0, 2.0), (400, 1.1), (650, 0.9), (800, 0.6), (1100, 0.5)),
     )
-    if ctx.speed < t_now / 24 * 300:
+    if ctx.speed < ctx.turn_count() / 24 * 300:
         spd *= 1.5
 
     sta = mathtools.integrate(
@@ -40,10 +32,9 @@ def compute(ctx: Context, trn: Training) -> float:
                 ctx.speed / 900 * 0.6 + 0.1 * ctx.date[0] if ctx.speed > 900 else 0.6,
             ),
             (900, ctx.speed / 900 * 0.3),
-            (1120, 0.1),
         ),
     )
-    pow_ = mathtools.integrate(
+    pow = mathtools.integrate(
         ctx.power,
         trn.power,
         (
@@ -51,45 +42,26 @@ def compute(ctx: Context, trn: Training) -> float:
             (300, 0.2 + ctx.speed / 600),
             (600, 0.1 + ctx.speed / 900),
             (900, ctx.speed / 900 / 3),
-            (1120, 0.1),
         ),
     )
-    gut = mathtools.integrate(
+    per = mathtools.integrate(
         ctx.guts,
         trn.guts,
-        (
-            (0, 2.0),
-            (300, 1.0 * ctx.speed / 400),
-            (400, 0.6 * ctx.speed / 400),
-            (600, 0.2 * ctx.speed / 400),
-            (900, 0.1 * ctx.speed / 400),
-            (1120, 0.1),
-        ),
+        ((0, 1.8), (300, 0.8), (400, 0.2), (600, 0.1))
+        if ctx.speed > 400 / 24 * ctx.turn_count()
+        else ((0, 2.0), (300, 0.5), (400, 0.1)),
     )
-    if ctx.guts > 300 and ctx.speed < min(1120, 400 / 24 * t_now):
-        gut *= 0.5
-
-    wis = mathtools.integrate(
+    int_ = mathtools.integrate(
         ctx.wisdom,
         trn.wisdom,
-        (
-            (0, 2.0),
-            (300, 0.8),
-            (600, 0.5),
-            (900, 0.3),
-            (1120, 0.1),
-        ),
+        ((0, 2.0), (400, 0.8), (550, 0.1)),
     )
-    if ctx.wisdom > 300 and ctx.speed < min(1120, 300 / 24 * t_now):
-        wis *= 0.1
 
-    vit = mathtools.clamp(trn.vitality, 0, 1 - ctx.vitality) * ctx.max_vitality * 0.6
+    vit = max(min(trn.vitality, 1 - ctx.vitality), 0) * ctx.max_vitality * 0.6
     if ctx.date[1:] in ((6, 1),):
         vit *= 1.2
     if ctx.date[1:] in ((6, 2), (7, 1), (7, 2), (8, 1)):
         vit *= 1.5
-    if ctx.date[0] == 4:
-        vit *= 0.3
 
     skill = trn.skill * 0.5
 
@@ -105,7 +77,7 @@ def compute(ctx: Context, trn: Training) -> float:
         pass
     elif trn.level < target_level:
         target_level_score += mathtools.interpolate(
-            t_now,
+            ctx.turn_count(),
             (
                 (0, 5),
                 (24, 3),
@@ -119,7 +91,7 @@ def compute(ctx: Context, trn: Training) -> float:
     fail_penality = 0
     if trn.type != trn.TYPE_WISDOM:
         fail_penality = mathtools.interpolate(
-            t_now,
+            ctx.turn_count(),
             (
                 (0, 30),
                 (72, 60),
@@ -129,7 +101,7 @@ def compute(ctx: Context, trn: Training) -> float:
     has_hint = any(i for i in trn.partners if i.has_hint)
     hint = 3 if has_hint else 0
     return (
-        (spd + sta + pow_ + gut + wis + skill + partner + target_level_score + hint)
+        (spd + sta + pow + per + int_ + skill + partner + target_level_score + hint)
         * success_rate
         + vit
         - fail_penality * trn.failure_rate
